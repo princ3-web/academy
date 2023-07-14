f25391c0caedc0f4dc727aaeb34de6887e3c22a8
Michal Gornicki <michal.gornicki@ithouse.co>
2023-07-14 15:33:21 +0200

    first commit

---
+++ .history/src/components/CourseCard_20230714153104.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import Image from "./Image";
+import { Typography } from "@mui/material";
+import StarRating from "./StarRating";
+import educationImage from "../assets/images/education.png";
+import CourseCardTooltip from "./CourseCardTooltip";
+import { teal } from "@mui/material/colors";
+import Button from "@mui/material/Button";
+
+const CourseCard = ({ boxWidth, width, height, imageObjectFit, title, text, rating }) => {
+  const [index, setIndex] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    setPosition({ x: event.clientX, y: event.clientY });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <Button sx={{ textTransform: "none", p: 0, m: 0 }}>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        position={"relative"}
+        width={boxWidth}
+        onMouseEnter={() => setIndex(true)}
+        onMouseLeave={() => setIndex(false)}
+        sx={{ cursor: "pointer", textAlign: "left" }}
+      >
+        <Image width={width} height={height} objectFit={imageObjectFit} src={educationImage} />
+        <Box height={150}>
+          <Typography fontSize={18} fontWeight={600}>
+            {title}
+          </Typography>
+          <Typography fontSize={14}>{text}</Typography>
+          <Box display={"flex"}>
+            <Typography color="darkorange" fontSize={16} fontWeight={600} mr={1}>
+              {rating}
+            </Typography>
+            <StarRating key={"starrating"} rating={rating} />
+          </Box>
+          <Typography fontSize={20} color={teal[600]} fontWeight={600}>
+            99,99 zł
+          </Typography>
+        </Box>
+        <Box>
+          <CourseCardTooltip
+            title={title}
+            text={text}
+            display={index ? "block" : "none"}
+            placement={position.x > window.innerWidth * 0.6}
+          />
+        </Box>
+      </Box>
+    </Button>
+  );
+};
+
+CourseCard.defaultProps = {
+  boxWidth: 200,
+  imageObjectFit: "cover",
+  title: "YYY",
+  text: "xxx",
+  rating: 3,
+};
+
+export default CourseCard;
---
+++ .history/src/components/StarRating_20230714153130.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+
+const StarRating = ({ rating }) => {
+  const stars = [];
+
+  // Create an array of five stars
+  for (let i = 1; i <= 5; i++) {
+    let starIcon;
+    if (i <= rating) {
+      starIcon = <StarRateIcon key={} color="warning" fontSize="small" />;
+    } else if (i - rating <= 0.5) {
+      starIcon = <StarHalfIcon color="warning" fontSize="small" />;
+    } else {
+      starIcon = <StarOutlineIcon color="warning" fontSize="small" />;
+    }
+
+    stars.push(starIcon);
+  }
+
+  return <div>{stars}</div>;
+};
+
+export default StarRating;
